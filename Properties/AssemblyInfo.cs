605f46fd1b17d12d0faeca10b4d9974ceb56b3b4
Ben Spiller <ben-spiller@users.noreply.github.com>
2017-04-11 10:48:07 +0100

    update version number to 1.1 and copyright for 2017

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Security;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BetterReminders")]
+[assembly: AssemblyTitle("Outlook BetterReminders")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BetterReminders")]
-[assembly: AssemblyCopyright("Copyright © Ben Spiller 2016")]
+[assembly: AssemblyProduct("Outlook BetterReminders")]
+[assembly: AssemblyCopyright("Copyright © Ben Spiller 2016-2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,6 +33,6 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 
